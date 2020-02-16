833dc2432f46ce1c0c199876cb42628171f8371a
meri <mdiaz>
2020-02-16 22:49:38 +0100

    Programa servver simple acabado

--- server_simple_mio.py
+++ server_simple_mio.py
@@ -10,6 +10,8 @@ alumna: Meritxell Diaz
 import socket
 import calculadora2
 
+Page = "HTTP/1.1 200 OK\r\n\r\n" + "<html><body><h1>Hello World!</h1><p>{}</p></body></html>" + "\r\n"
+
 # Create a TCP objet socket and bind it to a port
 # We bind to 'localhost', therefore only accepts connections from the
 # same machine
@@ -17,12 +19,15 @@ import calculadora2
 # let's use one above 1024
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind(('localhost', 1234))
+mySocket.bind(('localhost', 1236))
 
 # Queue a maximum of 5 TCP connection requests
 
 mySocket.listen(5)
 
+def sendsocket (cad, recvSocket):
+    recvSocket.send(bytes(Page.format(cad),"utf-8"))
+    recvSocket.close()
 # Accept connections, read incoming data, and answer back an HTML page
 #  (in an infinite loop)
 
@@ -30,43 +35,27 @@ while True:
     print('Waiting for connections')
     (recvSocket, address) = mySocket.accept()#le digo al socket que empiece a recibir
     print('HTTP request received:')
-    prueba = recvSocket.recv(1024)
+    peti = recvSocket.recv(1024)
 
-    print(prueba)
-    if str(prueba).find("GET") == -1:
-        print("HAY una str vacia")
+    if str(peti).find("GET") == -1:
+        #si estoy aqui es que me han puesto una peticion vacia, caso que ocurre
+        #siempre en la primera conexion
         continue
 
-    args = str(prueba).split(' ')
+    args = str(peti).split(' ')
+
     if args[1] == "/":
-        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
-                        b"<html><body><h1>Hello World!</h1></body></html>" +
-                        b"\r\n")
-        recvSocket.close()
+        sendsocket("", recvSocket)
         continue
-
+    #busco los argumentos de la calculadora
     args = args[1].split('/')
-    print(args)
-    if len(args) != 4:
-        #me han pasado algo malo
-        print("Error: Si quieres usar la calculadora debes poner funcion/op1/op2")
-        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
-                        b"<html><body><h1>Hello World!</h1><p>Si quieres usar la calculadora debes poner funcion/op1/op2</p></body></html>" +
-                        b"\r\n")
-        recvSocket.close()
-        continue
-    #si estoy aqui es porque ya puedo llmar a la calculadora
-    result = calculadora2.calcular(args[1], args[2], args[3])
 
-    if result == None:
-        #ha introducido mal los parametros o ha dividido por 0
-        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
-                        b"<html><body><h1>Hello World!</h1><p>Debes poner una operacion y operandos validos</p></body></html>" +
-                        b"\r\n")
-        recvSocket.close()
-    else:
-        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
-                        bytes("<html><body><h1>Hello World!</h1><p>El resultado es: {}</p></body></html>".format(result),"utf-8") +
-                        b"\r\n")
-        recvSocket.close()
-        print("El resultado es: "+ str(result))
+    if len(args) != 4:
+        #me han pasado algo malo para la calculadora
+        print("Error: Si quieres usar la calculadora debes poner funcion/op1/op2")
+        sendsocket("Si quieres usar la calculadora debes poner funcion/op1/op2", recvSocket)
+        continue
+    #si estoy aqui es porque ya puedo llamar a la calculadora con todos sus args
+    result = calculadora2.calcular(args[1], args[2], args[3])
+    sendsocket(result, recvSocket)
+    print(result)
--- servidor-http-simple.py
+++ servidor-http-simple.py
@@ -10,8 +10,6 @@ alumna: Meritxell Diaz
 import socket
 import calculadora2
 
-Page = "HTTP/1.1 200 OK\r\n\r\n" + "<html><body><h1>Hello World!</h1><p>{}</p></body></html>" + "\r\n"
-
 # Create a TCP objet socket and bind it to a port
 # We bind to 'localhost', therefore only accepts connections from the
 # same machine
@@ -19,15 +17,12 @@ Page = "HTTP/1.1 200 OK\r\n\r\n" + "<html><body><h1>Hello World!</h1><p>{}</p></
 # let's use one above 1024
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind(('localhost', 1236))
+mySocket.bind(('localhost', 1234))
 
 # Queue a maximum of 5 TCP connection requests
 
 mySocket.listen(5)
 
-def sendsocket (cad, recvSocket):
-    recvSocket.send(bytes(Page.format(cad),"utf-8"))
-    recvSocket.close()
 # Accept connections, read incoming data, and answer back an HTML page
 #  (in an infinite loop)
 
@@ -35,27 +30,43 @@ while True:
     print('Waiting for connections')
     (recvSocket, address) = mySocket.accept()#le digo al socket que empiece a recibir
     print('HTTP request received:')
-    peti = recvSocket.recv(1024)
+    prueba = recvSocket.recv(1024)
 
-    if str(peti).find("GET") == -1:
-        #si estoy aqui es que me han puesto una peticion vacia, caso que ocurre
-        #siempre en la primera conexion
+    print(prueba)
+    if str(prueba).find("GET") == -1:
+        print("HAY una str vacia")
         continue
 
-    args = str(peti).split(' ')
-
+    args = str(prueba).split(' ')
     if args[1] == "/":
-        sendsocket("", recvSocket)
+        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
+                        b"<html><body><h1>Hello World!</h1></body></html>" +
+                        b"\r\n")
+        recvSocket.close()
         continue
-    #busco los argumentos de la calculadora
-    args = args[1].split('/')
 
+    args = args[1].split('/')
+    print(args)
     if len(args) != 4:
-        #me han pasado algo malo para la calculadora
+        #me han pasado algo malo
         print("Error: Si quieres usar la calculadora debes poner funcion/op1/op2")
-        sendsocket("Si quieres usar la calculadora debes poner funcion/op1/op2", recvSocket)
+        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
+                        b"<html><body><h1>Hello World!</h1><p>Si quieres usar la calculadora debes poner funcion/op1/op2</p></body></html>" +
+                        b"\r\n")
+        recvSocket.close()
         continue
-    #si estoy aqui es porque ya puedo llamar a la calculadora con todos sus args
+    #si estoy aqui es porque ya puedo llmar a la calculadora
     result = calculadora2.calcular(args[1], args[2], args[3])
-    sendsocket(result, recvSocket)
-    print(result)
+
+    if result == None:
+        #ha introducido mal los parametros o ha dividido por 0
+        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
+                        b"<html><body><h1>Hello World!</h1><p>Debes poner una operacion y operandos validos</p></body></html>" +
+                        b"\r\n")
+        recvSocket.close()
+    else:
+        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
+                        bytes("<html><body><h1>Hello World!</h1><p>El resultado es: {}</p></body></html>".format(result),"utf-8") +
+                        b"\r\n")
+        recvSocket.close()
+        print("El resultado es: "+ str(result))
